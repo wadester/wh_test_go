dcdacd5e5ab66b0792c9409980288fab48232529
Wade Hampton <wadehamptoniv@gmail.com>
2015-09-29 11:23:35 -0400

    go_func1 created

---
+++ go_func1.go
@@ -0,0 +1,36 @@
+/*
+ * Module:   go_func1.go
+ * Purpose:  second simple examples of functions in go
+ * Date:     9/29/2015
+ * Notes:
+ * 1) To build:
+ *      go build go_func1.go
+*/
+package main
+
+import (
+	"fmt"
+)
+
+// Test function returing two args 
+// Note opening brace MUST be on same line as fcn due to auto 
+// ";" insertion so the following will not build:
+//      func swap(x, y string) (string, string) 
+//      {
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// Main function
+func main() {
+	var x string
+	y := string("World") // short dec form
+	x="Hello"
+	fmt.Println("X=", x)
+	fmt.Println("Y=", y)
+	// Define a,b using short declaration form := 
+	// Same as:
+	//   var a string; var b string; a, b = swap(x,y);
+	a, b := swap(x, y)
+	fmt.Println("swap", a, b)
+}
